102ae0e9236c6a1139d1c725a1975bc1201b3c88
eunjin21 <eunjin.k@21market.kr>
2023-01-31 14:32:10 +0900

    [Fix]

--- src/pages/Stock/Add.js
+++ src/pages/Stock/Add.js
@@ -167,7 +167,7 @@ const AddModal = ({ isOpen, onClose }) => {
       return;
     }
 
-    if (productData.price.slice(-2) != "00" || productData.price?.length > 1) {
+    if (productData.price.slice(-2) != "00" || productData.price?.length < 2) {
       setAlertText("백원단위 가격입력만 가능합니다.");
       setAlertModal(true);
       return;
