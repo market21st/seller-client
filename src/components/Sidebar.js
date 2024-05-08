b9351dad8d8854cb21a2acfeccbfb95ec204ad5e
yujin21market <yujin.h@21market.kr>
2024-05-08 14:32:21 +0900

    [UPDATE] 내 스토어 준비중

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -214,9 +214,7 @@ const Sidebar = ({ children }) => {
               <img src={localStorage.getItem("corpLogo")} alt="브랜드로고" />
             </div>
             <span>{localStorage.getItem("corpName")}</span>
-            <a href="https://www.21market.kr/" target="_blank">
-              내 스토어 바로가기
-            </a>
+            <a>내 스토어 준비중</a>
           </Profile>
           <Menu>
             <li>
